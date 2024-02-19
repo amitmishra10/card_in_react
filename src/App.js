30fdfefcab5885193a3e4215bb4fc27428d8ac99
Amit Mishra <amitmishraam941@gmail.com>
2024-02-19 17:17:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Itpages from "./pages/Itpages.js";
+import insta from "./assets/insta.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Itpages />
+      
+    </div>
+  );
+}
---
+++ src/pages/Itpages.js
@@ -0,0 +1,106 @@
+import "../styles/Itpages.css";
+import amit from "../assets/amit.jpg";
+function Itpages1(props) {
+  return (
+    <>
+      <div className="it-card">
+        <h3>{props.title}</h3>
+        <img src={props.src} />
+        <p>{props.description}</p>
+      </div>
+    </>
+  );
+}
+
+function Itpages() {
+  return (
+    <div className="App">
+      <h1>Main Page</h1>
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+      <hr />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+      <br />
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+      <br />
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+
+      <Itpages1
+        title="Itpages"
+        src={amit}
+        description="In cricket, eleven different men could play in eleven different ways and win a game in eleven different manners, unbound by a common playing theme, style or structure. Football, though played by eleven men and devouring massive real estate, contrarily to cricket, is bound by structures, styles, and philosophies. Thus, cricket has not had the equivalent of Catenaccio, Total Football, or Tiki Taka or Gegenpressing."
+      />
+    </div>
+  );
+}
+
+export default Itpages;
